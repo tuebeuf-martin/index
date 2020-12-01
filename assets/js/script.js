599db19f7ba2b1a7269b90d7c22aebcee4eab5d9
tuebeuf-martin <martin.tuebeuf@gmail.com>
2020-12-01 14:33:29 +0100

    Ajout de d'un systeme de score

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,24 @@
 /* First Push */
+$(document).ready(function(){
+    var score = 0;
+    var difficulty = 0;
+    $("#button").click(function(){ 
+        if (difficulty < 10){
+            score += 10;
+        }
+        else if (difficulty >= 10){
+            score += 20;
+        }
+        else if (difficulty >= 35){
+            score += 60;
+        }
+        else if (difficulty >= 50){
+            score += 100;
+        }
+        difficulty++;
+        $("#score").html(score);
+        console.log(difficulty)
+        console.log(score)
+    });
+});
+
